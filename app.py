b0d71d9ae6822c80d524a6010a54fd5490e9b1de
Avikal <69047714+Avikal-24@users.noreply.github.com>
2021-04-08 20:38:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
 import numpy as np
-from flask import Flask , request , jsonify , render_template
+from flask import Flask, render_template, request,jsonify
 import pickle
 
-app= Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
-# opening the pickle file in the read mode
-# model is ready now in the variable 'model'
+app=Flask(_name_)
+model=pickle.load(open('model.pkl','rb')) #loading pickle file in model variable
 
-@app.route('/')
+@app.route('/') #home route
 def home():
-    return render_template('index.html')
-
-@app.route('/predict' , methods=['POST'])
-def predict():
-    int_features= [int(x) for x in request.form.values()  ]
-    final_features = [np.array(int_features)]
-    predictions= model.predict(final_features)
-
-    output= round(predictions[0],2)
-    return render_template( 'index.html' , prediction_text= 'Employee salary should be $ {}'.format(output) )
+    return render_template('input.html')
+#predict route
 
 
-if __name__=='__main__':
+@app.route('/predict', methods=['POST'])
+def predict(): #for rendering ml model results on html gui
+
+    #int_features=[request.form['experience'],request.form['test_score'],request.form['interview_score']]
+    int_features=[int(x) for x in request.form.values()] #x is reading the 3 inputs from the form
+    #all 3 values(features) are available in int_features
+
+    final_features=[np.array(int_features)] #int_features converted into an array
+    prediction=model.predict(final_features) #used the model from model.py file for predictions
+    result=round(prediction[0],2) #rounds off a number to 2 decimal places
+    return render_template('input.html',prediction_text='Employee s salary= ${}'.format(result))
+
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if _name_ == "_main_":
     app.run(debug=True)
